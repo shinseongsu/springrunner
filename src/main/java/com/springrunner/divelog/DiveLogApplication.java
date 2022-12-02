7cc25b3b1bd3f94b1ba2c138fe71ad63f1b7d18d
Shin seongsu <tlstjdtn321@naver.com>
2022-12-02 23:30:33 +0900

    first commit

---
+++ src/main/java/com/springrunner/divelog/DiveLogApplication.java
@@ -0,0 +1,13 @@
+package com.springrunner.divelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiveLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springrunner/divelog/DiveLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrunner.divelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
